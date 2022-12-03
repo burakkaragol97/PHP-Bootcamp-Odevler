ee0f94b514e48e026d7fae7d084d6fc5d48fe1f6
Burak Karagöl <90074359+burakkaragol97@users.noreply.github.com>
2022-12-03 13:00:17 +0300

    Add files via upload

---
+++ PHP Bootcamp Bitirme Projesi - Öğrenci Yönetim Sistemi/yeni.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Öğrenci Kayıt</title>
+</head>
+<body style="text-align:center; padding-top:50px;">
+    <h1>Öğrenci Bilgilerini Giriniz</h1><br>
+    <form action="yeni.php" method="post">
+        <label for="isim">Öğrenci Adı ve Soyadını Giriniz</label><br>
+        <input type="text" name="isim" id="isim" required><br><br>
+        <label for="tckimlik">Öğrenci TC Kimlik Numarasını Giriniz</label><br>
+        <input type="text" name="tckimlik" id="tckimlik" required><br><br>
+        <label for="sinif">Öğrenci Sınıfını Giriniz</label><br>
+        <input type="text" name="sinif" id="sinif" required><br><br>
+        <label for="numara">Öğrenci Okul Numarasını Giriniz</label><br>
+        <input type="text" name="numara" id="numara" required><br><br>
+        <label for="cinsiyet">Öğrenci Cinsiyetini Giriniz</label><br>
+        <input type="text" name="cinsiyet" id="cinsiyet" required><br><br>
+        <label for="dogumtarih">Öğrencinin Doğum Tarihini Giriniz</label><br>
+        <input type="text" name="dogumtarih" id="dogumtarih" required><br><br>
+        <label for="vize1">Öğrencinin 1. Vize Notunu Giriniz</label><br>
+        <input type="text" name="vize1" id="vize1" required><br><br>
+        <label for="vize2">Öğrencinin 2. Vize Notunu Girinz</label><br>
+        <input type="text" name="vize2" id="vize2" required><br><br>
+        <label for="final">Öğrencinin Final Notunu Girniz</label><br>
+        <input type="text" name="final" id="final" required><br><br>
+        <button type="submit" name="submit">Kaydet</button>
+    </form>
+    <br>
+    <a href="liste.php">Liste Sayfası İçin Tıklayınız</a>
+</body>
+</html>
+
+<?php
+    require("baglan.php");
+
+    if(isset($_POST["submit"])) {
+        $isim = $_POST["isim"];
+        $tckimlik = $_POST["tckimlik"];
+        $sinif = $_POST["sinif"];
+        $numara = $_POST["numara"];
+        $cinsiyet = $_POST["cinsiyet"];
+        $dogum = $_POST["dogumtarih"];
+        $vize1 = $_POST["vize1"];
+        $vize2 = $_POST["vize2"];
+        $final = $_POST["final"];
+        $ortalama = ($vize1 + $vize2 + $final) / 3;
+
+        $sorgu = $baglan->prepare("insert into ogrenciler values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $ekle = $sorgu->execute(array(NULL, $isim, $tckimlik, $sinif, $numara, $cinsiyet, $dogum, $vize1, $vize2, $final));
+        
+        $toplam = $sorgu->rowCount();
+        if($toplam > 0) {
+            header("Refresh:3; url=liste.php");
+            echo
+            "<script> alert('Kayıt Eklendi: Öğrenciler Sayfasına Yönlendiriliyorsunuz (3 Saniye)'); </script>";
+        } else {
+            echo "Kayıt eklenemedi";
+        }
+    }
+?>
---
+++ PHP Bootcamp Bitirme Projesi - Öğrenci Yönetim Sistemi/yenisifre.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yeni Sifre</title>
+</head>
+<body>
+    <h1 style="text-align:center; padding-top: 100px;">Yeni Şifrenizi Giriniz</h1><br>
+    <form action="" method="post">
+        <div style="text-align:center;">
+            <label for="isim" ><strong>Kullanıcı Adınızı Giriniz</strong></label><br>
+            <input type="text" name="isim" id="isim"><br><br>
+            <label for="eskisifre" ><strong>Eski Şifrenizi Giriniz</strong></label><br>
+            <input type="password" name="eskisifre" id="eskisifre" ><br><br>
+            <label for="yenisifre" ><strong>Yeni Şifrenizi Giriniz</strong></label><br>
+            <input type="password" name="yenisifre" id="yenisifre" ><br><br>
+            <label for="sifrekontrol" ><strong>Yeni Şifrenizi Kontrol Edin</strong></label><br>
+            <input type="password" name="sifrekontrol" id="sifrekontrol"><br><br>
+            <button type="submit" name="submit">Kaydet</button>
+            <br><br>
+            <a href="login.php">Giriş Yapmak İçin Tıklayınız</a>
+        </div>
+    </form>
+</body>
+</html>
+
+<?php
+    require("baglan.php");
+    error_reporting(0);
+
+    $kullaniciAdi = $_POST["isim"];
+    $eskiSifre = $_POST["eskisifre"];
+    $yeniSifre = $_POST["yenisifre"];
+    $sifreKontrol = $_POST["sifrekontrol"];
+
+    if(isset($_POST["submit"])) {
+        $sorgu = $baglan->query("SELECT * FROM yonetici WHERE id = 1");
+        $satir = $sorgu->fetch(PDO::FETCH_ASSOC);
+        
+        if($kullaniciAdi == $satir["kullaniciadi"] && $eskiSifre == $satir["sifre"]) {
+            if($yeniSifre == $sifreKontrol) {
+                $degistir = $baglan->prepare("UPDATE yonetici SET sifre = ? WHERE sifre = ?");        
+                $degistir->execute(array($yeniSifre, $eskiSifre));
+                echo
+                "<script> alert('Şifre Başarıyla Değiştirildi')</script>;";
+            } else {
+                echo
+                "<script> alert('Parolalar Aynı Değil'); </script>";
+            }
+        } else {
+            echo
+            "<script> alert('Kullanıcı Bulunamadı') </script>";
+        }
+        
+                
+        
+
+
+
+
+
+
+
+
+
+
+                    
+             
+
+    }
+?>
